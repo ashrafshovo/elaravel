7390f81866e7366842417bb5a153552d34a1741c
Ashraf Hossan Shovo <ashrafshovo@gmail.com>
2019-05-16 04:28:30 +0600

    Update SuperAdminController.php

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Redirect;
 class SuperAdminController extends Controller
 {
     //
+    public function index()
+    {
+    	$this->AdminAuthCheck();
+    	return view('pages.back.dashboard');
+    }
+
     public function logout()
     {
     	//Session::put('admin_id', null);
@@ -18,4 +24,15 @@ class SuperAdminController extends Controller
     	
     	return Redirect::to( route('login') );
     }
+
+    public function AdminAuthCheck()
+    {
+    	$admin_id = Session::get('admin_id');
+    	if ($admin_id) {
+    		return;
+    	} else{
+    		return redirect(route('login'))->send();
+    	}
+    }
+
 }
