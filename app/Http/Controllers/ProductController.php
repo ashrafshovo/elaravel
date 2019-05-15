546b74bbe534af71eb38fd63d86c61a899aba95f
Ashraf Hossan Shovo <ashrafshovo@gmail.com>
2019-05-16 02:40:29 +0600

    Updated Product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use Carbon\Carbon;
+use Brian2694\Toastr\Facades\Toastr;
 
 class ProductController extends Controller
 {
@@ -19,6 +20,7 @@ class ProductController extends Controller
         $products = DB::table('tbl_products')
                 ->join('tbl_categories', 'tbl_products.category_id', '=', 'tbl_categories.category_id')
                 ->join('tbl_manufacture', 'tbl_products.manufacture_id', '=', 'tbl_manufacture.manufacture_id')
+                ->select('tbl_products.*', 'tbl_categories.category_name', 'tbl_manufacture.manufacture_name')
                 ->get();
 
     /*  
@@ -105,7 +107,8 @@ class ProductController extends Controller
 
         DB::table('tbl_products')->insert($data);
 
-        return redirect( route('product.index') )->with('successMsg', 'Product successfully added.');
+        Toastr::success('Product successfully added.', 'Success', ["positionClass" => "toast-top-right"]);
+        return redirect( route('product.index') )/*->with('successMsg', 'Product successfully added.')*/;
     }
 
     /**
@@ -129,10 +132,19 @@ class ProductController extends Controller
     {
         //
         $product = DB::table('tbl_products')
+                ->join('tbl_categories', 'tbl_products.category_id', '=', 'tbl_categories.category_id')
+                ->join('tbl_manufacture', 'tbl_products.manufacture_id', '=', 'tbl_manufacture.manufacture_id')
+                ->select('tbl_products.*', 'tbl_categories.category_name', 'tbl_manufacture.manufacture_name')
                 ->where('product_id', $id)
                 ->first();
 
-        return view('admin.product.edit', compact('product'));
+        $categories = DB::table('tbl_categories')
+                    ->get();
+
+        $manufactures = DB::table('tbl_manufacture')
+                    ->get();
+
+        return view('admin.product.edit', compact('product', 'categories', 'manufactures'));
     }
 
     /**
@@ -145,6 +157,66 @@ class ProductController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $this->validate( $request, [
+            'product_name' => 'required|string',
+            'category_id' => 'required',
+            'manufacture_id' => 'required',
+            'product_short_description' => 'required',
+            'product_long_description' => 'required',
+            'product_price' => 'required',
+            'product_color' => 'required'
+        ]);
+
+
+
+        $data = array();
+        $data['product_name'] = $request->product_name;
+        $data['category_id'] = $request->category_id;
+        $data['manufacture_id'] = $request->manufacture_id;
+        $data['product_short_description'] = $request->product_short_description;
+        $data['product_long_description'] = $request->product_long_description;
+        $data['product_price'] = $request->product_price;
+        
+        $data['product_size'] = $request->product_size;
+        $data['product_color'] = $request->product_color;
+        
+        $image = $request->file('product_image');
+        $slug = str_slug($request->product_name);
+
+        $product = DB::table('tbl_products')
+                ->where('product_id', $id)
+                ->first();
+
+        if (isset($image)) {
+            $currentDate = Carbon::now()->toDateString();
+            $ext = $image->getClientOriginalExtension();
+            $imageName = $slug.'-'.$currentDate.'-'.uniqid().'.'.$ext;
+
+            if (!file_exists('uploads/product')) {
+                mkdir('uploads/product', 0777, true);
+            }
+            if (file_exists('uploads/product/'.$product->product_image)) {
+                unlink('uploads/product/'. $product->product_image);
+            }
+            $image->move('uploads/product', $imageName);
+        } else {
+            $imageName = $product->product_image;
+        }
+
+        $data['product_image'] = $imageName;
+    
+    /*
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    */
+
+        DB::table('tbl_products')
+            ->where('product_id', $id)
+            ->update($data);
+
+        Toastr::success('Product successfully updated.', 'Success', ["positionClass" => "toast-top-right"]);
+        return redirect( route('product.index') )/*->with('successMsg', 'Product successfully updated.')*/;
     }
 
     /**
@@ -160,6 +232,7 @@ class ProductController extends Controller
         $product = DB::table('tbl_products')
                 ->where('product_id', $id)
                 ->first();
+
         if (file_exists('uploads/product/'.$product->product_image)) {
             unlink('uploads/product/'.$product->product_image);
         }
@@ -167,6 +240,9 @@ class ProductController extends Controller
         DB::table('tbl_products')
                 ->where('product_id', $id)
                 ->delete();
+
+        Toastr::success('Product successfully deleted.', 'Success', ["positionClass" => "toast-top-right"]);
+        
         return redirect(route('product.index'))->with('successMsg', 'Product deleted successfully.');
     }
 
--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -25,28 +25,90 @@
 				<h2><i class="halflings-icon edit"></i><span class="break"></span>Edit Product</h2>
 			</div>
 			<div class="box-content">
-				<form class="form-horizontal" action="{{ route('product.update', $product->product_id) }}" method="post">
+				<form class="form-horizontal" action="{{ route('product.update', $product->product_id) }}" method="post" enctype="multipart/form-data">
 					@csrf
 					@method('put')
 					<fieldset>
 						<div class="control-group">
 							<label class="control-label" for="date01">Product Name</label>
-								<div class="controls">
-									<input type="text" class="input-xlarge" name="product_name" value="{{ $product->product_name }}" required>
-							 	</div>
-						</div>
-						<div class="control-group hidden-phone">
-							<label class="control-label" for="textarea2">Product Description</label>
 							<div class="controls">
-								<textarea class="cleditor" name="product_short_description" id="textarea2" rows="0" required>{{ $product->product_short_description }}</textarea>
+								<input type="text" class="input-xlarge" name="product_name" value="{{ $product->product_name }}" required>
+							</div>
+						</div>
+
+						<div class="control-group">
+							<label class="control-label" for="selectError">Product Category</label>
+							<div class="controls">
+								<select id="selectError3" name="category_id">
+									<option value="{{ $product->category_id }}">{{ $product->category_name }}</option>
+									@foreach($categories as $category)
+										<option value="{{ $category->category_id }}">{{ $category->category_name }}</option>
+									@endforeach
+								</select>
+							</div>
+						</div>
+
+						<div class="control-group">
+							<label class="control-label" for="selectError">Manufacture Name</label>
+							<div class="controls">
+								<select id="selectError3" name="manufacture_id">
+									<option value="{{ $product->manufacture_id }}" >{{ $product->manufacture_name }}</option>
+									@foreach($manufactures as $manufacture)
+										<option value="{{ $manufacture->manufacture_id }}" >{{ $manufacture->manufacture_name }}</option>
+									@endforeach
+								</select>
+							</div>
+						</div>
+
+						<div class="control-group hidden-phone">
+							<label class="control-label" for="textarea2">Product Short Description</label>
+							<div class="controls">
+								<textarea class="cleditor" name="product_short_description" id="textarea1" rows="0" required>{{ $product->product_short_description }}</textarea>
+							</div>
+						</div>
+
+						<div class="control-group hidden-phone">
+							<label class="control-label" for="textarea2">Product Long Description</label>
+							<div class="controls">
+								<textarea class="cleditor" name="product_long_description" id="textarea2" rows="0" required>{{ $product->product_long_description }}</textarea>
+							</div>
+						</div>
+
+						<div class="control-group">
+							<label class="control-label" for="appendedPrependedInput">Product Price</label>
+							<div class="controls">
+								<div class="input-prepend">
+									<span class="add-on">৳</span><input id="appendedPrependedInput" size="16" type="text" value="{{ $product->product_price }}" name="product_price" required>
+								</div>
+							</div>
+						</div>
+
+						<div class="control-group">
+							<label class="control-label" for="date01">Product Image</label>
+							<div class="controls">
+								<input type="file" name="product_image" >
+							</div>
+						</div>
+
+						<div class="control-group">
+							<label class="control-label" for="date01">Product Size</label>
+							<div class="controls">
+								<input type="text" class="input-xlarge" name="product_size" value="{{ $product->product_size }}" >
+							</div>
+						</div>
+
+						<div class="control-group">
+							<label class="control-label" for="date01">Product Color</label>
+							<div class="controls">
+								<input type="text" class="input-xlarge" name="product_color" value="{{ $product->product_color }}" required>
 							</div>
 						</div>
 
 						<div class="form-actions">
 							<button type="submit" class="btn btn-primary">Update Product</button>
-							<a href="{{ URL::previous() }}" class="btn">Cancel</a>
+							<button type="reset" class="btn">Cancel</button>
 						</div>
-					</fieldset>
+				 	</fieldset>
 				</form>
 			</div>
 		</div><!--/span-->
