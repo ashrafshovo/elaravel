521cde3e3d3511366128ff811fd0e81f36931445
Ashraf Hossan Shovo <ashrafshovo@gmail.com>
2019-05-25 00:18:40 +0600

    Create 2019_05_23_174357_create_slider_table.php

---
+++ database/migrations/2019_05_23_174357_create_slider_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSliderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_slider', function (Blueprint $table) {
+            $table->increments('slider_id');
+            $table->string('slider_title');
+            $table->string('slider_image');
+            $table->text('slider_description');
+            $table->tinyInteger('publication_status')->default(0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_slider');
+    }
+}
